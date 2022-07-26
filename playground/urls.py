8fa37095c961cfae67cae692959be044722ab379
andrewlim99 <andrew.l@agiletechnica.com>
2022-07-26 19:46:42 +0700

    Create page for get all movies

--- playground/urls.py
+++ playground/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home_page)
+    path('home/', views.home_page),
+    path('test/', views.get_movie),
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+import requests
+
 
 # Create your views here.
 def home_page(request):
     return render(request, 'index.html')
+
+
+def get_movie(request):
+    api_key = "08a4cc722f0c09e91f63172cac468dc6"
+    movie_data_list = []
+    
+    headers = {'Content-Type': 'application/json'}
+    body = {
+        "page": 1,
+        "api_key": api_key,
+        "language": "English"
+    }
+    get_movie_list_response = requests.get('https://api.themoviedb.org/4/list/1/', headers=headers, params=body).json()
+    for i in get_movie_list_response['results']:
+        response_url = 'https://image.tmdb.org/t/p/w500/{poster_url}'.format(poster_url=i['poster_path'])
+        movie_data_list.append({
+            'poster_path': response_url,
+            'title': i['original_title'],
+            'overview': i['overview']
+        })
+    print(movie_data_list)
+    return render(request, 'index.html', {"movie_list": movie_data_list})
